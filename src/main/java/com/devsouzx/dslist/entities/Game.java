0cd86f945a168e02f656aaa952a4ebff2f1e5d1c
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2024-11-14 11:31:00 -0300

    properties, Game entity, ORM

---
+++ src/main/java/com/devsouzx/dslist/entities/Game.java
@@ -0,0 +1,33 @@
+package com.devsouzx.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+}
